30e91e7d8c34372d1cd54e957a65b9bb074eaebe
SergeyTsiganovskiy <stsyg@softserveinc.com>
2018-09-08 17:19:21 +0300

    added model classes for requests

--- automation/aautomation_framework/PageObjects/CommonControls.cs
+++ automation/aautomation_framework/PageObjects/CommonControls.cs
@@ -13,5 +13,27 @@ namespace aautomation_framework.PageObjects
         {
             WebDriver.Navigate().Refresh();
         }
+
+        public void NavigateBack()
+        {
+            WebDriver.Navigate().Back();
+        }
+
+        public string GetCurrentUrl()
+        {
+            return WebDriver.Url;
+        }
+
+        public static void ExpandGridWidth(IWebDriver webDriver)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)webDriver;
+            js.ExecuteScript("document.getElementById('center').style.width = '1000000000px';");
+        }
+
+        public void LogOut()
+        {
+            WaitForElementIsClickable(null).Click();
+            WaitForElementIsClickable(null).Click();
+        }
     }
 }
--- automation/aautomation_framework/PageObjects/TemplatePageObject.cs
+++ automation/aautomation_framework/PageObjects/TemplatePageObject.cs
@@ -13,6 +13,13 @@ namespace aautomation_framework.PageObjects
         
         private By SomeLocator1 = By.XPath("//div[@colid = 'submission_type']");
         private By SomeLocator2(string value1, string value2) => By.XPath($"//div[@colid = '{value1}{value2}submission_type']");
+        private By SomeLocator3 = By.XPath("//span[contains(text(),'Hide')]//ancestor::label/input");
+        private By SomeLocator4(string column) => By.XPath($"//*[(normalize-space()='{column}')]");
+        private By SomeLocator5 = By.XPath(".//div[@colid='product_name']//div[@class='info-icon ng-isolate-scope']");
+        private By SomeLocator6 = By.XPath("//*[@data-sat='mc-input-smartsearch-form-control-static-is-array-of-objects-input']");
+        private By SomeLocator7 = By.XPath("");
+        private By SomeLocator8 = By.XPath("");
+        private By SomeLocator9 = By.XPath("");
 
         #endregion locators
 
